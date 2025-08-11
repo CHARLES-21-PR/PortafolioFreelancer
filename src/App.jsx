c1ae551be11d5915752d6b97c159860511b1d124
LorTha0123 <lorenatlq338018@gmail.com>
2025-08-10 22:09:05 -0500

    segundo avance

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navegation from './Components/Navegation';
+import titulo from './Components/componente_titulo/tit_comp';
 import './App.css'
 
 function App() {
@@ -10,8 +11,18 @@ function App() {
   return (
     <>
     <Navegation/>
+
     </>
+
   )
+  return (
+      <>
+
+      <titulo/>
+      </>
+
+    )
+
 }
 
 export default App
---
+++ src/Components/componente_titulo/tit_comp.jsx
@@ -0,0 +1,29 @@
+
+function titulo() {
+
+    return (
+
+        <div className='nav'>
+            <h1>Mi Portafolio</h1>
+        </div>
+    );
+}
+
+export default titulo;
+
+
+/*const tit_comp = ()=> {
+    return(
+        <div>Mi Portafolio<div>
+    );
+
+}
+
+const App = ()=> {
+
+    return(
+        <div><tit_comp></tit_comp></div>
+    );
+}
+export default App;
+*/
