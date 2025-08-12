116b5389caaf424e49c383d189ff0c9fdf617659
CHARLES-21PR <2301010144@undc.edu.pe>
2025-08-11 19:09:53 -0500

    vCorregida4

--- src/Components/componente_titulo/tit_comp.jsx
+++ src/Components/componente_titulo/tit_comp.jsx
@@ -12,19 +12,4 @@ function Tit_comp() {
 export default Tit_comp;
 
 
-/*const tit_comp = ()=> {
-    return(
-        <div>Mi Portafolio<div>
-    );
-
-}
-
-const App = ()=> {
-
-    return(
-        <div><tit_comp></tit_comp></div>
-    );
-}
-export default App;
-*/
 
