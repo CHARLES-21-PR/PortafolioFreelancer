b6bb993feb035627d2257334d8575cdba920258a
CHARLES-21PR <2301010144@undc.edu.pe>
2025-08-16 18:34:47 -0500

    v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Navegation from './Components/Navegation';
-import Tit_comp from './Components/componente_titulo/Tit_comp';
+import Navegation from './Components/Navegation'
+import Tit_comp from './Components/componente_titulo/tit_comp'
 import './App.css'
 
 function App() {
