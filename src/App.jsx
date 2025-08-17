061c4f3843166d82d2608a11e43d8cd6b3510357
CHARLES-21PR <2301010144@undc.edu.pe>
2025-08-16 19:10:25 -0500

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navegation from './Components/Navegation';
-import Tit_comp from './Components/componente_titulo/Tit_comp';
+import Tit_comp from './Components/componente_titulo/tit_comp';
 import Card_comp from   './Components/componente_tarjeta/Card_comp';
-import Tarjeta2 from   './Components/componente_tarjeta/Card_comp';
+import CardBody_comp from './Components/componente_tarjeta/CardBody_comp'
 import './App.css'
 
 function App() {
   return (
     <>
-    <Navegation/>
-    <Tit_comp />
-    <Card_comp/>
-    <Tarjeta2/>
+    <div className="app-container">
+      <Navegation/>
+      <div className="main-content">
+        <Tit_comp />
+        <div className="cards">
+          <Card_comp/>
+          <CardBody_comp/>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
--- src/Components/componente_tarjeta/Card_comp.jsx
+++ src/Components/componente_tarjeta/Card_comp.jsx
@@ -4,22 +4,22 @@ function Card_comp() {
     return (
         <div className="tarjeta">
             <img src="/img/perfil.png" className="tarjeta-img" />
-            <h2>Carlos Adrian HM</h2>
+            <h2>FreelancerGroup</h2>
             <p>Front-end Developer</p>
         </div>
 
     );
 }
-function Tarjeta2() {
-    return (
-        <div className="tarjeta">
-            <img src="/img/perfil.png" className="tarjeta-img" />
-            <h2>Romel Denis AQ </h2>
-            <p>Aprendiz de todo, maestro de nada</p>
-        </div>
-    );
+// function Tarjeta2() {
+//     return (
+//         <div className="tarjeta">
+//             <img src="/img/perfil.png" className="tarjeta-img" />
+//             <h2>Romel Denis AQ </h2>
+//             <p>Aprendiz de todo, maestro de nada</p>
+//         </div>
+//     );
 
-}
+// }
 
 export default Card_comp;
 
